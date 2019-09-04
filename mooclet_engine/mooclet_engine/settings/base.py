e36a5051638a7c7b670c1fa9bc60e938e2899667
Samuel Maldonado <sam.maldonado@cognitivescience.co>
2019-09-04 15:13:12 -0400

    add pagination

--- mooclet_engine/mooclet_engine/settings/base.py
+++ mooclet_engine/mooclet_engine/settings/base.py
@@ -184,6 +184,8 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 #### cors headers ####
