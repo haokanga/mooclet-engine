8925cdc2ef911d5e4463add4264ee5d0c74f4066
Samuel Maldonado <sam.maldonado@cognitivescience.co>
2020-02-12 19:05:58 -0500

    minor fix and edge case adjustment for non contextual TS

--- mooclet_engine/engine/policies.py
+++ mooclet_engine/engine/policies.py
@@ -73,37 +73,43 @@ def thompson_sampling(variables,context):
 	#priors we set by hand - will use instructor rating and confidence in future
 	# TODO : all explanations are having the same prior.
 
-    # context is the following json :
-    #   {
-    #   'policy_parameters':
-    #       {
-    #       'outcome_variable_name':<name of the outcome variable',
-    #       'max_rating': <maximum value of the outcome variable>,
+	# context is the following json :
+	#   {
+	#   'policy_parameters':
+	#       {
+	#       'outcome_variable_name':<name of the outcome variable',
+	#       'max_rating': <maximum value of the outcome variable>,
 	#       'prior':
-    #           {'success':<prior success value>},
-    #           {'failure':<prior failure value>},
-    #       }
-    #   }
-	prior_success = context['policy_parameters']['prior']['success']
+	#           {'success':<prior success value>},
+	#           {'failure':<prior failure value>},
+	#       }
+	#   }
+	policy_parameters = context["policy_parameters"].parameters
 
-	prior_failure = context['policy_parameters']['prior']['failure']
-	outcome_variable_name = context['policy_parameters']['outcome_variable_name']
+	prior_success = policy_parameters['prior']['success']
+
+	prior_failure = policy_parameters['prior']['failure']
+	outcome_variable_name = policy_parameters['outcome_variable_name']
 	#max value of version rating, from qualtrics
-	max_rating = context['policy_parameters']['max_rating']
+	max_rating = policy_parameters['max_rating']
 
 	version_to_show = None
 	max_beta = 0
 
 	for version in versions:
-		student_ratings = Variable.objects.get(name=outcome_variable_name).get_data({'version': version}).all()
-        # student_ratings is a pandas.core.series.Series variable
-		rating_count = student_ratings.count()
-		rating_average = student_ratings.aggregate(Avg('value'))
-		rating_average = rating_average['value__avg']
-		if rating_average is None:
+		student_ratings = Variable.objects.get(name=outcome_variable_name).get_data({'version': version})
+		if student_ratings:
+			student_ratings = Variable.objects.get(name=outcome_variable_name).get_data({'version': version}).all()
+		    # student_ratings is a pandas.core.series.Series variable
+			rating_count = student_ratings.count()
+			rating_average = student_ratings.aggregate(Avg('value'))
+			rating_average = rating_average['value__avg']
+			if rating_average is None:
+				rating_average = 0
+
+		else: 
 			rating_average = 0
-
-
+			rating_count = 0
 		#get instructor conf and use for priors later
 		#add priors to db
 		# prior_success_db, created = Variable.objects.get_or_create(name='thompson_prior_success')
