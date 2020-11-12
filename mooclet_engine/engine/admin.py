82df3dbb31331cecf3df04d129c4e84e91b3b548
Samuel Maldonado <sam.maldonado@cognitivescience.co>
2020-11-12 12:41:21 -0500

    add policyparamhistory

--- mooclet_engine/engine/admin.py
+++ mooclet_engine/engine/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+class PolicyParameterHistoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('creation_time',)
 admin.site.register(Environment)
 admin.site.register(Mooclet)
 admin.site.register(Version)
@@ -10,4 +13,4 @@ admin.site.register(Value)
 admin.site.register(Policy)
 admin.site.register(Learner)
 admin.site.register(PolicyParameters)
-admin.site.register(PolicyParametersHistory)
+admin.site.register(PolicyParametersHistory, PolicyParameterHistoryAdmin)
