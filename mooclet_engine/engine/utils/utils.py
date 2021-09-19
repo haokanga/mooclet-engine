6952e2f0f42281f4d337cb78a92bbe83cc2531e9
VictorS67 <185000048@qq.com>
2021-09-19 15:38:22 -0400

    add print statements for debugging

--- mooclet_engine/engine/utils/utils.py
+++ mooclet_engine/engine/utils/utils.py
@@ -146,7 +146,7 @@ def values_to_df(mooclet, policyparams, latest_update=None):
                     print("values_to_df: action: {}, {}".format(action, type(action)))
                     print("values_to_df: value.version: {}".format(value.version))
                     print("values_to_df: value.version.version_json: {}".format(value.version.version_json))
-                    print("values_to_df: value.version.version_json[is_arm5]: {}".format(value.version.version_json["is_arm5"]))
+                    print("values_to_df: value.version.version_json[is_arm5_round_1]: {}".format(value.version.version_json["is_arm5_round_1"]))
                     # print("current_version_json: ")
                     # print(value.version.version_json)
                     curr_action_config = value.version.version_json[action]
