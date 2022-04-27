c1480aba30e28f49a663e5da5272242e55900b26
Jiakai Shi <jiakai.shi@mail.utoronto.ca>
2022-04-27 00:01:01 -0400

    minor change

--- mooclet_engine/engine/urls.py
+++ mooclet_engine/engine/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
 	url(r'^data', views.PandasValueViewSet.as_view()),
 	url(r'^learner-data', views.PandasLearnerValueViewSet.as_view()),
 	# url(r'^api/getBinaryContextualImputer/', views.getBinaryContextualImputer.as_view()),
-	url(r'^api/initContext/', views.getContextualImputer.as_view())
+	url(r'^api/initContext/', views.ContextualImputer.as_view())
 ]
