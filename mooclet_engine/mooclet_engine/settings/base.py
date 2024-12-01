6aa9eeb44da5b26006871f0b41dfeff42fac860a
Marshall An <haokanga@andrew.cmu.edu>
2024-12-01 17:01:29 -0500

    Revert "standard Django convention for BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))"
    This reverts commit e46e79cf6822225427daebc05cca48b7cd492aa5.

--- mooclet_engine/mooclet_engine/settings/base.py
+++ mooclet_engine/mooclet_engine/settings/base.py
@@ -17,7 +17,7 @@ from . import secure
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE DIR refers to the directory containing manage.py
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Absolute filesystem path to the Django project config directory:
 # (this is the parent of the directory where this file resides,
